bdcb40726363b8694c98bbb57f575f21e44cfe48
pipelinetode <zach@pipelinetode.com>
2024-07-27 12:19:52 -0400

    Add configuration file

---
+++ news_api/config.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+today = datetime.now()
+
+# INSERT API KEY HERE.
+
+# api_key = ""
+
+str_cols = ["author", "title"]
+
+# If storing creds in GCP
+
+# bucket = "news_api_cred_store"
+
+# creds = "news_api_creds.json"
